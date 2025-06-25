85c109e63df229d4bfb56ae061e308c464885e08
RoeiBason <roeibason1234@gmail.com>
2025-06-25 17:07:30 +0300

    added changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Box, Typography } from '@mui/material';
-import Post from './Features/Post/Post';
 import HomePage from './Features/HomePage/HomePage';
 import NavBar from './Features/ApplicationLayout/NavBar';
 
--- my-app/src/Features/ApplicationLayout/NavBar.tsx
+++ my-app/src/Features/ApplicationLayout/NavBar.tsx
@@ -1,19 +1,18 @@
-import React, { FC } from "react";
-import { AppBar, Toolbar, IconButton, Typography, Box, Stack, Button } from "@mui/material";
-import { CatchingPokemon } from "@mui/icons-material";
-import KeremNetLogo from './KeremNetLogo.png'
 import './NavBar.css'
+import React, { FC } from "react";
+import { AppBar, Toolbar,  Typography, Box, Stack, Button } from "@mui/material";
+import KeremNetLogo from './KeremNetLogo.png'
 
 const NavBar: FC = () => {
   return (
-    <AppBar className='appBar'>
+    <AppBar className='navBarPageHeader'>
       <Toolbar className='toolBar'>
         <Box className='logo' component="img" alt="KeremNet Logo" src={KeremNetLogo} />
         <Typography className='appTitle' variant="h6" component="div" >
           KeremNet
         </Typography>
-        <Stack
-          className='navStack'>
+        <Stack className='navStack'>
+          <Button className='navButton'>Posts</Button>
           <Button className='navButton'>Posts</Button>
         </Stack>
       </Toolbar>
