564ac3a2f11f1bff1e01bb883e6bb2b132e44b33
RoeiBason <roeibason1234@gmail.com>
2025-06-25 17:53:27 +0300

    fixed all problems with style in NavBar

--- my-app/src/Features/ApplicationLayout/NavBar.tsx
+++ my-app/src/Features/ApplicationLayout/NavBar.tsx
@@ -1,19 +1,19 @@
 import './NavBar.css'
 import React, { FC } from "react";
-import { AppBar, Toolbar,  Typography, Box, Stack, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Stack, Button } from "@mui/material";
 import KeremNetLogo from './KeremNetLogo.png'
 
 const NavBar: FC = () => {
   return (
-    <AppBar className='navBarPageHeader'>
+    <AppBar sx={{ bgcolor: '#800080', position: 'static' }}>
       <Toolbar className='toolBar'>
         <Box className='logo' component="img" alt="KeremNet Logo" src={KeremNetLogo} />
-        <Typography className='appTitle' variant="h6" component="div" >
+        <Typography variant="h6" component="div" flexGrow={1} >
           KeremNet
         </Typography>
-        <Stack className='navStack'>
-          <Button className='navButton'>Posts</Button>
-          <Button className='navButton'>Posts</Button>
+        <Stack className='navStack' flexDirection={'row'}>
+          <Button sx={{ color: '#FFFFFF' }} >Posts</Button>
+          <Button sx={{ color: '#FFFFFF' }}>Posts</Button>
         </Stack>
       </Toolbar>
     </AppBar>
